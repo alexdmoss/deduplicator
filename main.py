1f9423597a3ce77241781d684f95e5113f751176
Alex M <alex@mosstech.io>
2020-10-03 14:40:37 +0100

    Bash/comments tidy up

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 #
-# ----------------------------------------------------------------------------------------
-#
-# usage: find-duplicates.py [-h] [-d DIR] [-o OUTPUT] [-r] [-v] [-q]
-#
-#           (-h for full description of args)
-#
-# ----------------------------------------------------------------------------------------
-#
-# Reads from a single directory supplied through command line arguments - if you want to
-# search across multiple directories you will need to either move or symlink to them.
-#
-# Option (--remove) to DELETE the last found duplicate (so order of folders matters).
-#
-# Option (--output) to write out duplicate files to a file to then do something else with.
+# main.py --dir=DIR
+#        [--output=OUTPUT]
+#        [--delete]         # removes last found duplicate only - order of folders matters
+#        [--debug]
+#        [--quiet]
 
 import sys
 from deduplicator.start import start
