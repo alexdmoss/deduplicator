4610f70682de8541033f44de9894482ff54e8620
Alex M <alex@alexos.dev>
2023-12-12 11:29:12 +0000

    Attempt to speed things up a bit

--- deduplicator/files.py
+++ deduplicator/files.py
@@ -1,7 +1,17 @@
 import os
+import concurrent.futures
+from dataclasses import dataclass
+from time import time
+import hashlib
+import random
 
 from deduplicator.logger import log
-from deduplicator.utils import hash_file
+
+
+@dataclass
+class FileResult:
+    file: str
+    hash: str
 
 
 def list_all_files(folder: str) -> dict:
@@ -14,17 +24,34 @@ def list_all_files(folder: str) -> dict:
     return all_files
 
 
+def hash_files_parallel(files, num_processes=None):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count() if num_processes is None else num_processes) as executor:
+        for result in executor.map(hash_file, files):
+            if result is not None:
+                yield result
+
+
 def hash_files(list_of_files):
     hashed_files = {}
     log.info("Hashing list of files")
-    for file in list_of_files:
-        file_hash = hash_file(file)
-        log.debug(f"Hash of file {file} was {file_hash}")
-        if file_hash in hashed_files:
-            hashed_files[file_hash].append(file)
-        else:
-            hashed_files[file_hash] = [file]
+
+    for result in hash_files_parallel(list_of_files):
+        if result is not None:
+
+            if result.hash not in hashed_files:
+                hashed_files[result.hash] = [result.file]
+            else:
+                hashed_files[result.hash].append(result.file)
+
     return hashed_files
+    # for file in list_of_files:
+    #     file_hash = hash_file(file)
+    #     log.debug(f"Hash of file {file} was {file_hash}")
+    #     if file_hash in hashed_files:
+    #         hashed_files[file_hash].append(file)
+    #     else:
+    #         hashed_files[file_hash] = [file]
+    # return hashed_files
 
 
 def delete_duplicates(results: dict):
@@ -42,3 +69,22 @@ def find_duplicate_hashes(all_files: dict) -> dict:
         if len(files) > 1:
             duplicates[md] = files
     return duplicates
+
+
+def hash_file(path, blocksize=2**22):
+    m = hashlib.sha256()
+    try:
+        with open(path, "rb") as f:
+            while True:
+                buf = f.read(blocksize)
+                if not buf:
+                    break
+                m.update(buf)
+        log.debug(f"Hashed {path}")
+        return FileResult(file=path, hash=m.hexdigest())
+    except FileNotFoundError:
+        log.error("File not found: " + path)
+        return FileResult(file=path, hash= time() + random.randint(1, 1000000000))
+    except OSError:  # pragma: no cover (@TODO: mocking doesn't seem to work)
+        log.error("File could not be opened: " + path)
+        return FileResult(file=path, hash= time() + random.randint(1, 1000000000))
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,4 +1,17 @@
-from deduplicator.files import find_duplicate_hashes, list_all_files, hash_files, delete_duplicates
+from deduplicator.files import find_duplicate_hashes, list_all_files, hash_files, delete_duplicates, hash_file
+
+
+def test_hash_file():
+    test_input_file = "./mocks/files/image-with-dupe-L0.jpg"
+    digest = hash_file(test_input_file)
+    assert digest.hash == "e8114e843bc16f5e895d6aa752bf3584"
+    assert digest.file == "./mocks/files/image-with-dupe-L0.jpg"
+
+
+def test_hash_file_missing(caplog):
+    test_missing_file = "./mocks/not-there.jpg"
+    hash_file(test_missing_file)
+    assert 'File not found' in caplog.text
 
 
 def test_list_all_files():
